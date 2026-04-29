dfc3fb90220c65440049ab0d4784de59a61e235f
Imranshaik0434 <imranshaik.aws@gmail.com>
2026-04-29 17:29:35 +0530

    Refactor deployment script in testDeploy.groovy
    Updated the deployment script to fix syntax and improve clarity.

--- vars/testDeploy.groovy
+++ vars/testDeploy.groovy
@@ -1,8 +1,20 @@
-def call(tomcatIp, tomcat-dev){
+/* def call(tomcatIp, tomcat-dev){
 sshagent([tomcat-dev]){
                     sh 'scp -o StrictHostKeyChecking=no target/oniline-banking.war ec2-user@tomcatIp:/opt/tomcat11/webapps/'
                     sh 'ls -lrt'
                     sh 'ssh -o StrictHostKeyChecking=no ec2-user@tomcatIp "/opt/tomcat11/bin/shutdown.sh"'
                     sh 'ssh -o StrictHostKeyChecking=no ec2-user@tomcatIp "/opt/tomcat11/bin/startup.sh"'
                 }
+}
+*/
+
+def call(tomcatIp, tomcat-dev) {
+    sshagent([tomcat-dev]) {
+        sh """
+        scp -o StrictHostKeyChecking=no target/online-banking.war ec2-user@${tomcatIp}:/opt/tomcat11/webapps/
+        ls -lrt
+        ssh -o StrictHostKeyChecking=no ec2-user@${tomcatIp} "/opt/tomcat11/bin/shutdown.sh"
+        ssh -o StrictHostKeyChecking=no ec2-user@${tomcatIp} "/opt/tomcat11/bin/startup.sh"
+        """
+    }
 }
