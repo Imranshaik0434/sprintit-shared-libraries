6d11e55143995eeed7cd29cacbffc91b5d1abafa
Imranshaik0434 <imranshaik.aws@gmail.com>
2026-04-29 17:24:33 +0530

    Fix sshagent syntax in testDeploy.groovy

--- vars/testDeploy.groovy
+++ vars/testDeploy.groovy
@@ -1,5 +1,5 @@
 def call(tomcatIp, tomcat-dev){
-sshagent([tomcat-dev]) {
+sshagent([tomcat-dev]){
                     sh 'scp -o StrictHostKeyChecking=no target/oniline-banking.war ec2-user@tomcatIp:/opt/tomcat11/webapps/'
                     sh 'ls -lrt'
                     sh 'ssh -o StrictHostKeyChecking=no ec2-user@tomcatIp "/opt/tomcat11/bin/shutdown.sh"'
