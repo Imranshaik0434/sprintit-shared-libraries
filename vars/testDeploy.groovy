50dad21cd620fea54f4c2cf3b4c302edf4363666
Imranshaik0434 <imranshaik.aws@gmail.com>
2026-04-29 17:13:47 +0530

    Add deployment script for Tomcat server

---
+++ vars/testDeploy.groovy
@@ -0,0 +1,8 @@
+def call(tomcatIp, tomcat-dev){
+sshagent([tomcat-dev]) {
+                    sh 'scp -o StrictHostKeyChecking=no target/oniline-banking.war ec2-user@tomcatIp:/opt/tomcat11/webapps/'
+                    sh 'ls -lrt'
+                    sh 'ssh -o StrictHostKeyChecking=no ec2-user@tomcatIp "/opt/tomcat11/bin/shutdown.sh"'
+                    sh 'ssh -o StrictHostKeyChecking=no ec2-user@tomcatIp "/opt/tomcat11/bin/startup.sh"'
+                }
+}
